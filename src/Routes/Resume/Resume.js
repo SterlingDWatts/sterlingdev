2f24278a44fa220337d3958152521fa90654d3f0
Sterling Watts <SterlingDWatts@gmail.com>
2020-05-22 13:17:37 -0700

    Add og title to resume page

--- src/Routes/Resume/Resume.js
+++ src/Routes/Resume/Resume.js
@@ -8,6 +8,7 @@ class Resume extends Component {
       <div className="Resume">
         <Helmet>
           <title>Sterling | Resume</title>
+          <meta property="og:title" content="Sterling | Dev" />
         </Helmet>
         <div className="Resume--top">
           <section className="Resume--hero">
